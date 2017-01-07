18c6df996ae0bfc0214724b02e7e936843cb1a36
Julien Schueller <schueller@phimeca.com>
2017-01-07 17:52:04 +0100

    Use SHLIB_EXT

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if test `uname` = "Darwin"
-then
-  SHLIB_EXT='.dylib'
-else
-  SHLIB_EXT='.so'
-fi
-
 PY_LIB=`find $PREFIX/lib -name libpython${PY_VER}*${SHLIB_EXT}`
 
 $PYTHON setup.py build --extra-fortran-link-flags="${PY_LIB}"
