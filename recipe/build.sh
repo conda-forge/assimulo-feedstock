b7b2f89bb651e1718137abd2150c7d163eeb56c2
Julien Schueller <schueller@phimeca.com>
2017-03-13 22:30:04 +0100

    Remove useless flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-PY_LIB=`find ${PREFIX}/lib -name libpython${PY_VER}*${SHLIB_EXT}`
-
-${PYTHON} setup.py install --extra-fortran-link-flags=${PY_LIB} --sundials-home=${PREFIX}
+${PYTHON} setup.py install --sundials-home=${PREFIX}
