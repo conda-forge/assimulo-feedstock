364dbb5963ba1af0cf0427361685391c7b18ca2d
Julien Schueller <schueller@phimeca.com>
2017-03-14 08:38:48 +0100

    Copy instead of patching

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+if test `uname` = "Darwin"
+then
+  # copy instead of patching because of problematic line endings
+  cp ${RECIPE_DIR}/assimulo.solvers.__init__.py assimulo/solvers/__init__.py
+fi
+
 ${PYTHON} setup.py install --sundials-home=${PREFIX}
