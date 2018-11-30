1e884ac3b03ee5e839fed06961e63264626e50c5
Julien Schueller <schueller@phimeca.com>
2018-11-30 12:03:19 +0100

    Force cython rebuild

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -23,6 +23,8 @@ if errorlevel 1 exit 1
 patch -p0 -i %RECIPE_DIR%/r845.patch --binary
 if errorlevel 1 exit 1
 
+rem force cython rebuild
+del assimulo\*.c assimulo\solvers\*.c
 
 "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
