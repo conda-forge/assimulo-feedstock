71b6af54160e3a5a7be62224218d741bb2c218a0
Julien Schueller <schueller@phimeca.com>
2019-01-09 08:58:15 +0100

    Update to 3.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,24 +4,6 @@ patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
 if errorlevel 1 exit 1
 
 
-patch -p0 -i %RECIPE_DIR%/r831.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r833.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r836.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r837.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r838.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r839.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r840.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r841.patch --binary
-if errorlevel 1 exit 1
-patch -p0 -i %RECIPE_DIR%/r845.patch --binary
-if errorlevel 1 exit 1
 
 rem force cython rebuild
 del assimulo\*.c assimulo\solvers\*.c
