b26f3a26aa94d8ffcc2dd196d82a9054341d9691
Julien Schueller <schueller@phimeca.com>
2019-01-11 09:37:31 +0100

    Win: enable fortran extensions

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,4 @@
 
-rem eol endings errors with recent conda build
-patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
-if errorlevel 1 exit 1
-
 rem force cython rebuild
 del assimulo\*.c assimulo\solvers\*.c
 
