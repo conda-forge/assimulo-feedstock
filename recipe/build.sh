82298cc3fc8ef81027f2416f154f0580ccbf2480
Julien Schueller <schueller@phimeca.com>
2019-01-11 09:07:45 +0100

    -shared

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-${PYTHON} setup.py install --sundials-home=${PREFIX} --extra-fortran-link-flags="-shared" --blas-home=${PREFIX}/lib --blas-name="openblas" --lapack-home=${PREFIX}/lib
+${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --blas-name=openblas --lapack-home=${PREFIX}/lib
