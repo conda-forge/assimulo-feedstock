6f31a0b317d91a62659d74eecc572164fb7be938
Julien Schueller <schueller@phimeca.com>
2019-01-11 11:06:08 +0100

    CC=mingw32

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 rem force cython rebuild
 del assimulo\*.c assimulo\solvers\*.c
 
-set CC=gcc
+set CC=mingw32
 
 %PYTHON% setup.py install --sundials-home=%LIBRARY_PREFIX% --no-msvcr True
 if errorlevel 1 exit 1
