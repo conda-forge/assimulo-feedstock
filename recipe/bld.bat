595473c51cb6393137f104144f7967e81f896299
Julien Schueller <schueller@phimeca.com>
2019-01-11 11:14:53 +0100

    remove vc

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,5 @@
 rem force cython rebuild
 del assimulo\*.c assimulo\solvers\*.c
 
-set CC=mingw32
-
 %PYTHON% setup.py install --sundials-home=%LIBRARY_PREFIX% --no-msvcr True
 if errorlevel 1 exit 1
