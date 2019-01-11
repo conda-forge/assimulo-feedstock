655200eea13e3c854cc37728edda4cce40a6c99d
Julien Schueller <schueller@phimeca.com>
2019-01-11 10:38:48 +0100

    CC=gcc

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,5 +2,7 @@
 rem force cython rebuild
 del assimulo\*.c assimulo\solvers\*.c
 
-"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX%
+set CC=gcc
+
+%PYTHON% setup.py install --sundials-home=%LIBRARY_PREFIX% --no-msvcr True
 if errorlevel 1 exit 1
