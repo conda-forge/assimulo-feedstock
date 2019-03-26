8cf785db78b54997a44031a38e0c37f7f374c74c
Julien Schueller <schueller@phimeca.com>
2019-03-26 14:53:20 +0100

    distutils

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,16 @@ rem eol endings errors with recent conda build
 rem patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
 rem if errorlevel 1 exit 1
 
+set CFG=%USERPROFILE%\pydistutils.cfg
+echo [config] > "%CFG%"
+echo compiler=mingw32 >> "%CFG%"
+echo [build] >> "%CFG%"
+echo compiler=mingw32 >> "%CFG%"
+echo [build_ext] >> "%CFG%"
+echo compiler=mingw32 >> "%CFG%"
+
 rem force cython rebuild
 del assimulo\*.c assimulo\solvers\*.c
 
-"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --compiler=mingw32
+"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
