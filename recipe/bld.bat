9e3b5b07e3564d83e0ea7b599bc8873792af9b8e
Julien Schueller <schueller@phimeca.com>
2019-03-26 16:18:02 +0100

    No need to remove c files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,5 @@ rem eol endings errors with recent conda build
 patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
 if errorlevel 1 exit 1
 
-rem force cython rebuild
-del assimulo\*.c assimulo\solvers\*.c
-
 "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
