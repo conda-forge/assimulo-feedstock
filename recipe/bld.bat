111b9dc1bd7798ab95542409f250df721e253d00
Julien Schueller <schueller@phimeca.com>
2019-03-26 16:34:34 +0100

    Use flang

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 
 rem eol endings errors with recent conda build
-patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
-if errorlevel 1 exit 1
+rem  patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
+rem  if errorlevel 1 exit 1
 
 "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
