5854d74128347053621de95e3f1e5e430e8b8720
Julien Schueller <schueller@phimeca.com>
2019-03-26 14:39:36 +0100

    Try mingw

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,10 @@
 
 rem eol endings errors with recent conda build
-patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
-if errorlevel 1 exit 1
+rem patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
+rem if errorlevel 1 exit 1
 
 rem force cython rebuild
 del assimulo\*.c assimulo\solvers\*.c
 
-"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX%
+"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --compiler=mingw32
 if errorlevel 1 exit 1
