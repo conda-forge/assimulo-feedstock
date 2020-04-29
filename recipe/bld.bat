1fcb3f30eba7055a075e1084a84c69df4293cbde
Julien Schueller <schueller@phimeca.com>
2020-04-29 18:41:48 +0200

    Dir

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,6 +10,11 @@ rem eol endings errors with recent conda build
 rem patch -p1 -i %RECIPE_DIR%/disable-fortran-extensions.patch --binary
 if errorlevel 1 exit 1
 
+
+
 "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+dir /p %SP_DIR%
 if errorlevel 1 exit 1
 
