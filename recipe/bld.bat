01b7a0ed09ccf6d2e235eff802d86698d7b30248
Julien Schueller <schueller@phimeca.com>
2021-11-09 08:29:06 +0100

    6126688

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 
 :: sundials 5.x support
 git config --global user.email "you@example.com" && git config --global user.name "Your Name"
-git remote add js https://github.com/jschueller/Assimulo.git && git fetch js && git cherry-pick 0b07e2fd9f3810fe5ab72e477907e4b50618c1f3
+git cherry-pick 6126688246d28775d11bf74039f91c7066b8de04
 if errorlevel 1 exit 1
 
 "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib
