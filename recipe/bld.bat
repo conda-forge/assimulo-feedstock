71ed4faf215c407e1e8b9a73f31c69588d862396
Julien Schueller <schueller@phimeca.com>
2021-12-03 13:27:28 +0100

    Update to 3.2.8

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,4 @@
 
-:: sundials 5.x support
-git config --global user.email "you@example.com" && git config --global user.name "Your Name"
-git cherry-pick 6126688246d28775d11bf74039f91c7066b8de04
-if errorlevel 1 exit 1
-
 "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib
 if errorlevel 1 exit 1
 
