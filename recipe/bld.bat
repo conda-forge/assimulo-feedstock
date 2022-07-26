1845a305d46fac2a32a64983ec56166066fd9e53
Julien Schueller <schueller@phimeca.com>
2022-07-26 09:49:08 +0200

    libs

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,3 @@
 "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib
 if errorlevel 1 exit 1
 
-:: missing dlls
-xcopy %SP_DIR%\Assimulo-%PKG_VERSION%-py%PY_VER%-win-amd64.egg\assimulo\.libs\*.dll %SP_DIR%\Assimulo-%PKG_VERSION%-py%PY_VER%-win-amd64.egg\assimulo\lib
-if errorlevel 1 exit 1
