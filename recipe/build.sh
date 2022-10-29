f3168cff7a63941887c253141a5df6300e8a0983
Julien Schueller <schueller@phimeca.com>
2022-10-29 08:04:06 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if test `uname` = "Darwin"
-then
-  ${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --lapack-home=${PREFIX}/lib
-else
-  ${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --lapack-home=${PREFIX}/lib --extra-fortran-compile-flags="-fallow-argument-mismatch"
-fi
-
+${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --lapack-home=${PREFIX}/lib --extra-fortran-compile-flags="-fallow-argument-mismatch"
