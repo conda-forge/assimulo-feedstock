4b55b7eaa6c1cb8f8e85211d63ef3ce2bf0a9f7b
Julien Schueller <schueller@phimeca.com>
2023-06-06 10:18:33 +0200

    superlu

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --lapack-home=${PREFIX}/lib --extra-fortran-compile-flags="-fallow-argument-mismatch"
+# local superlu install
+curl -fSsL https://portal.nersc.gov/project/sparse/superlu/superlu_mt_3.1.tar.gz | tar xz
+cd SuperLU_MT_3.1
+if test `uname` = "Linux"
+then
+  MPLIB="-fopenmp"
+fi
+make CC=${CC} CFLAGS="-O2 -fPIC ${MPLIB}" BLASLIB="-lblas" PLAT="_OPENMP" MPLIB=${MPLIB} lib -j1
+mv SRC include
+cd -
+
+${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --lapack-home=${PREFIX}/lib --extra-fortran-compile-flags="-fallow-argument-mismatch" --superlu-home=${PWD}/SuperLU_MT_3.1
