8939af4642313e3282188445e08f2397bc3ccb07
Julien Schueller <schueller@phimeca.com>
2023-07-22 11:57:57 +0200

    Enable slu (#81)
    * Enable slu
    
    * link1
    
    * link2
    
    * link3
    
    * link4
    
    * cvode_with_jac_sparse

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,13 @@
 
-"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib
+git clone -b fix https://github.com/jschueller/superlu_mt.git
+cmake -LAH -G "Ninja" -B build_slu -S superlu_mt ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_INSTALL_PREFIX="%SRC_DIR:\=/%/install_slu" ^
+  -DSUPERLUMT_INSTALL_INCLUDEDIR="%SRC_DIR:\=/%/install_slu/include" ^
+  -DPLAT="_OPENMP" -DBUILD_SHARED_LIBS=OFF
+cmake --build build_slu --target install --config Release
+
+"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib --superlu-home=%SRC_DIR%\install_slu
 if errorlevel 1 exit 1
 
 :: missing dlls
