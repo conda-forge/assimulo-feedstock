3a63d607667219d3681de71adbbe55bcddd5f9ff
Julien Schueller <schueller@phimeca.com>
2023-07-22 07:26:52 +0200

    link1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ cmake -LAH -G "Ninja" -B build_slu -S superlu_mt ^
   -DPLAT="_OPENMP" -DBUILD_SHARED_LIBS=OFF
 cmake --build build_slu --target install --config Release
 
-"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib --superlu-home=%SRC_DIR%\install_slu --extra-c-flags="/D__OPENMP"
+"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib --superlu-home=%SRC_DIR%\install_slu
 if errorlevel 1 exit 1
 
 :: missing dlls
