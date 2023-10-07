beda5cd631ba3ceb37e9f708afb25c533264ccb3
Julien Schueller <schueller@phimeca.com>
2023-10-07 11:57:32 +0200

    CMAKE_INSTALL_LIBDIR

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ git clone --depth 1 https://github.com/xiaoyeli/superlu_mt.git
 cmake -LAH -G "Ninja" -B build_slu -S superlu_mt \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${SRC_DIR}/install_slu \
+  -DCMAKE_INSTALL_LIBDIR=${SRC_DIR}/install_slu/lib \
   -DSUPERLUMT_INSTALL_INCLUDEDIR=${SRC_DIR}/install_slu/include \
   -DPLAT="_OPENMP" -DBUILD_SHARED_LIBS=OFF
 cmake --build build_slu --target install
