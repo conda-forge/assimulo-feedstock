a743c79f7409b000ae9f72fe3098ab29b6bac50c
Julien Schueller <schueller@phimeca.com>
2024-05-17 18:45:17 +0200

    cmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,16 +2,25 @@
 :: local superlu install
 git clone -b v4.0.1 --depth 1 https://github.com/xiaoyeli/superlu_mt.git
 cmake -LAH -G "Ninja" -B build_slu -S superlu_mt ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX:\=/% ^
   -DCMAKE_INSTALL_PREFIX="%SRC_DIR:\=/%/install_slu" ^
   -DSUPERLUMT_INSTALL_INCLUDEDIR="%SRC_DIR:\=/%/install_slu/include" ^
   -DPLAT="_OPENMP" -DBUILD_SHARED_LIBS=OFF -Denable_tests=OFF -Denable_examples=OFF
 cmake --build build_slu --target install
 
-"%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib --superlu-home=%SRC_DIR%\install_slu
-if errorlevel 1 exit 1
+rem  "%PYTHON%" setup.py install --sundials-home=%LIBRARY_PREFIX% --blas-home=%LIBRARY_PREFIX%\lib --lapack-home=%LIBRARY_PREFIX%\lib --superlu-home=%SRC_DIR%\install_slu
+rem  if errorlevel 1 exit 1
+rem  
+rem  :: missing dlls
+rem  xcopy %SP_DIR%\assimulo\.libs\*.dll %SP_DIR%\assimulo\lib
+rem  if errorlevel 1 exit 1
 
-:: missing dlls
-xcopy %SP_DIR%\assimulo\.libs\*.dll %SP_DIR%\assimulo\lib
-if errorlevel 1 exit 1
+git clone -b skbuild https://github.com/jschueller/Assimulo.git
+cmake -LAH -G "Ninja" -B build -S Assimulo ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX:\=/% ^
+  -DCMAKE_INSTALL_PATH=%LIBRARY_PREFIX:\=/% ^
+  -DPYTHON_SITE_PACKAGE=%SP_DIR:\=/% ^
+  -DSUPERLUMT_LIBRARY=%SRC_DIR:\=/%/install_slu/lib/superlu_mt_OPENMP.lib ^
+  -DSUPERLUMT_INCLUDE_DIR=%SRC_DIR:\=/%/install_slu/include
+cmake --build build --target install
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 # local superlu install
 git clone -b v4.0.1 --depth 1 https://github.com/xiaoyeli/superlu_mt.git
-cmake -LAH -G "Ninja" -B build_slu -S superlu_mt \
+cmake ${CMAKE_FLAGS} -LAH -G "Ninja" -B build_slu -S superlu_mt \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${SRC_DIR}/install_slu \
   -DCMAKE_INSTALL_LIBDIR=${SRC_DIR}/install_slu/lib \
@@ -11,7 +11,19 @@ cmake -LAH -G "Ninja" -B build_slu -S superlu_mt \
 cmake --build build_slu --target install
 
 # some logs are missing
-sed -i.bak 's|L.warning|print|g' setup.py
-sed -i.bak 's|L.debug|print|g' setup.py
+# sed -i.bak 's|L.warning|print|g' setup.py
+# sed -i.bak 's|L.debug|print|g' setup.py
+# 
+# ${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --lapack-home=${PREFIX}/lib --extra-fortran-compile-flags="-fallow-argument-mismatch" --superlu-home=${SRC_DIR}/install_slu
 
-${PYTHON} setup.py install --sundials-home=${PREFIX} --blas-home=${PREFIX}/lib --lapack-home=${PREFIX}/lib --superlu-home=${SRC_DIR}/install_slu
+
+
+
+git clone -b skbuild https://github.com/jschueller/Assimulo.git
+cmake ${CMAKE_FLAGS} -LAH -G "Ninja" -B build -S Assimulo \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PATH=${PREFIX} \
+  -DSUPERLUMT_LIBRARY=${SRC_DIR}/install_slu/lib/libsuperlu_mt_OPENMP.a \
+  -DSUPERLUMT_INCLUDE_DIR=${SRC_DIR}/install_slu/include
+cmake --build build --target install
+
